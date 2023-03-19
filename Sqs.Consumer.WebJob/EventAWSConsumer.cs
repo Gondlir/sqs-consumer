7edcb85e510cc3f927577de7fac34819ca1c3464
Kaoe Ferreira <kaoeskt@hotmail.com>
2023-03-18 23:14:59 -0300

    criado o serviço de leitura das mensagens da fila

---
+++ Sqs.Consumer.WebJob/EventAWSConsumer.cs
@@ -0,0 +1,62 @@
+﻿using Amazon;
+using Amazon.Runtime.Internal.Endpoints.StandardLibrary;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Sqs.Consumer.WebJob.Models;
+using System.Runtime.InteropServices;
+
+namespace Sqs.Consumer.WebJob
+{
+    public sealed class EventAWSConsumer
+    {
+        private static string _url = "customers";
+        private static AmazonSQSClient _awsClient = new AmazonSQSClient(RegionEndpoint.USEast1);
+        private static CustomerCreatedMessage _customerMessage;
+        private static CancellationTokenSource _cancelationToken = new CancellationTokenSource();
+        public async static Task MainAsync()
+        {
+            Console.WriteLine("Iniciando consumo da mensagem !");
+            var queueUrlReponse = await _awsClient.GetQueueUrlAsync(_url);
+            await ProcessQueueCustomerMessageAsync(queueUrlReponse);
+        }
+
+        #region Private Methods
+        private async static Task ProcessQueueCustomerMessageAsync(GetQueueUrlResponse queueUrlReponse)
+        {
+            var receivedMessageRequest = new ReceiveMessageRequest
+            {
+                QueueUrl = queueUrlReponse.QueueUrl,
+                // por default, aws não envia os atributos para poder ter mais eficiência no envio de mensagens
+                AttributeNames = new List<string> { "All" },
+                MessageAttributeNames = new List<string> { "All" }
+            };
+
+            while (!_cancelationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var response = await _awsClient.ReceiveMessageAsync(receivedMessageRequest, _cancelationToken.Token);
+                    //if (response.Messages is null || response.Messages.Count == 0)
+                    //    return;
+
+                    foreach (var message in response.Messages)
+                    {
+                        Console.WriteLine($"Message Id: { message.MessageId }");
+                        Console.WriteLine($"Message Body: { message.Body }");
+                        // delete messages from queue
+                        await _awsClient.DeleteMessageAsync(queueUrlReponse.QueueUrl, message.ReceiptHandle);
+                    }
+                    await Task.Delay(3000);
+                }
+                catch (Exception ex)
+                {
+                    // Log errors
+                    Console.WriteLine("Houve algum erro ! ");
+                    throw;
+                }
+                
+            }
+        }
+        #endregion
+    }
+}
--- Sqs.Consumer.WebJob/Program.cs
+++ Sqs.Consumer.WebJob/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Sqs.Consumer.WebJob;
+
 Console.WriteLine("Hello, World!");
+await EventAWSConsumer.MainAsync();
